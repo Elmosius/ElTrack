9acb8da21c6a9bb62d14b69d974a439fd107f921
Elmosius Suli <elmosius18@gmail.com>
2026-03-25 12:38:50 +0700

    feat: make mock page login

--- src/routes/_auth/auth.login.tsx
+++ src/routes/_auth/auth.login.tsx
@@ -1,9 +1,26 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Button } from '#/components/selia/button';
+import { Card, CardBody, CardDescription, CardHeader, CardTitle } from '#/components/selia/card';
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_auth/auth/login')({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/auth/login"!</div>
+  return (
+    <>
+      <Card className='ring-0 text-center shadow-none'>
+        <CardHeader className='border-none'>
+          <CardTitle className='font-semibold text-2xl lg:text-4xl tracking-wide py-1'>Welcome to ElTrack</CardTitle>
+          <CardDescription className='text-sm lg:text-base'>Please login to your account to continue using ElTrack and manage your financial records effectively.</CardDescription>
+        </CardHeader>
+        <CardBody className='p-0'>
+          <Button variant={'outline'} className={'text-sm lg:text-base'}>
+            <img src='/web_neutral_rd_na.svg' alt='Google Logo' className='size-8' />
+            Continue with Google
+          </Button>
+        </CardBody>
+      </Card>
+    </>
+  );
 }
