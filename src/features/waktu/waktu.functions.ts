bd82b5b4bebab2ee6ede5c20aee5476e58a31278
Elmosius Suli <elmosius18@gmail.com>
2026-03-28 16:06:58 +0700

    delete: db & features waktu

--- src/features/waktu/waktu.functions.ts
+++
@@ -1,6 +0,0 @@
-import { createServerFn } from '@tanstack/react-start';
-import { listWaktu } from './waktu.server';
-
-export const getListWaktu = createServerFn({ method: 'GET' }).handler(async () => {
-  return listWaktu();
-});
