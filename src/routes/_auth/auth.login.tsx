405e8004d623dcfa223c398c8d0498e5bd69605a
Elmosius Suli <elmosius18@gmail.com>
2026-06-29 16:42:59 +0700

    fix: user had login can login again

--- src/routes/_auth/auth.login.tsx
+++ src/routes/_auth/auth.login.tsx
@@ -17,12 +17,12 @@ function RouteComponent() {
 
   return (
     <>
-      <Card className='ring-0 text-center shadow-none bg-background'>
-        <CardHeader className='border-none justify-center flex flex-col items-center'>
-          <CardTitle className='font-semibold text-3xl tracking-wide py-1'>Welcome to ElTrack</CardTitle>
-          <CardDescription className='text-sm lg:text-base md:w-2/3'>Please login to your account to continue using ElTrack and manage your financial records effectively.</CardDescription>
+      <Card className='w-full max-w-md ring-0 text-center shadow-none bg-background'>
+        <CardHeader className='flex! grid-cols-none! flex-col items-center justify-center gap-2 border-none p-0!'>
+          <CardTitle className='font-semibold text-2xl md:text-3xl tracking-wide py-1'>Welcome to ElTrack</CardTitle>
+          <CardDescription className='text-sm lg:text-base max-w-sm'>Please login to your account to continue using ElTrack and manage your financial records effectively.</CardDescription>
         </CardHeader>
-        <CardBody className='p-0'>
+        <CardBody className='mt-6 p-0!'>
           <Button variant={'outline'} className={'text-sm lg:text-base bg-white'} onClick={handleGoogleLogin}>
             <img src='/web_neutral_rd_na.svg' alt='Google Logo' className='size-8' />
             Continue with Google
--- src/routes/_auth/route.tsx
+++ src/routes/_auth/route.tsx
@@ -1,8 +1,16 @@
 import AuthLayout from '#/components/layouts/auth';
-import { createFileRoute, Outlet } from '@tanstack/react-router';
+import { getSession } from '#/lib/auth/functions';
+import { createFileRoute, Outlet, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_auth')({
   component: RouteComponent,
+  beforeLoad: async () => {
+    const session = await getSession();
+
+    if (session) {
+      throw redirect({ to: '/', search: { month: undefined } });
+    }
+  },
 });
 
 function RouteComponent() {
